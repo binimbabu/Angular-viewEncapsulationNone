b0b7ee2add95a875ba76ecad9030ee28c3cd3ba1
Bini M Babu <binibabu22@gmail.com>
2024-06-12 11:25:06 +0530

    view encapsulation none

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'viewencapsulationNone';
+  //https://www.youtube.com/watch?v=X-1TBjBx6pc
 }
